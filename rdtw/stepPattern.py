cb4ae7d85634439cf53c8c8cfe281eb241bdd0bb
Toni <toni.giorgino@gmail.com>
2019-08-23 14:17:46 +0200

    pattern building

--- rdtw/stepPattern.py
+++ rdtw/stepPattern.py
@@ -75,12 +75,106 @@ def _c(*v):
     return(va)
     
 
+##################################################
+##################################################
+
+# Reimplementation of the building process
+
+class _P:
+    def __init__(self, pid, subtype, smoothing):
+        self.subtype = subtype
+        self.smoothing = smoothing
+        self.pid = pid
+        self.i = [0]
+        self.j = [0]
+
+    def addStep(self, di, dj):  # equivalent to .Pstep
+        self.i.append(di)
+        self.j.append(dj)
+        return self
+
+    def getMx(self):             # eqv to .Pend
+        ia = numpy.array(self.i)
+        ja = numpy.array(self.j)
+        si = numpy.cumsum(ia)
+        sj = numpy.cumsum(ja)
+        ni = numpy.max(si)-si # ?
+        nj = numpy.max(sj)-sj
+        if self.subtype == "a":
+            w = numpy.minimum(ia, ja)
+        elif self.subtype == "b":
+            w = numpy.maximum(ia, ja)
+        elif self.subtype == "c":
+            w = ia
+        elif self.subtype == "d":
+            w = ia+ja
+        else:
+            error("Unsupported subtype")
+
+        if self.smoothing:
+            w = numpy.mean(w[1:])
+
+        w[0] = -1.0
+
+        nr = len(w)
+        mx = numpy.zeros((nr,4))
+        mx[:,0] = self.pid
+        mx[:,1] = ni
+        mx[:,2] = nj
+        mx[:,3] = w
+        return mx
+
+
+def rabinerJuangStepPattern(ptype, slope_weighting="d", smoothed=False):
+    f = {
+        1: _RJtypeI,
+        # 2: _RJtypeII,
+        # 3: _RJtypeIII,
+        # 4: _RJtypeIV,
+        # 5: _RJtypeV,
+        # 6: _RJtypeVI,
+        # 7: _RJtypeVII
+    }.get(ptype, lambda: error("Invalid type"))
+
+    r = f(slope_weighting, smoothed)
+    norm = "NA"
+    if slope_weighting=="c":
+        norm = "N"
+    elif slope_weighting == "d":
+        norm = "N+M"
+
+    return StepPattern(r, norm)
+
+
+def _RJtypeI(s,m):
+    return numpy.vstack( [
+        _P(1, s, m).addStep(1,0).getMx(),
+        _P(2, s, m).addStep(1,1).getMx(),
+        _P(3, s, m).addStep(0,1).getMx() ] )
+
 
     
+##########################################################################################
+##########################################################################################
+
+## Everything here is semi auto-generated from the R source. Don't
+## edit!
+
+
 ##################################################
 ##################################################
 
-## Everything here is auto-generated from the R source
+
+##
+## Various step patterns, defined as internal variables
+##
+## First column: enumerates step patterns.
+## Second   	 step in query index
+## Third	 step in reference index
+## Fourth	 weight if positive, or -1 if starting point
+##
+## For \cite{} see dtw.bib in the package
+##
 
 
 
