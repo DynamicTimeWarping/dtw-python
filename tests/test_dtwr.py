ed0b9c13120a86092c3cb418989c75d727231b44
Toni <toni.giorgino@gmail.com>
2019-08-27 17:59:04 +0200

    test cli

--- tests/test_dtwr.py
+++ tests/test_dtwr.py
@@ -25,12 +25,14 @@ class TestRDTW(unittest.TestCase):
     def test_command_line_interface(self):
         """Test the CLI."""
         runner = CliRunner()
-        result = runner.invoke(cli.main)
+        result = runner.invoke(cli.main,
+                               '--query tests/query.csv --reference tests/reference.csv'.split())
         assert result.exit_code == 0
-        assert 'dtwr.cli.main' in result.output
+        assert '0.1292' in result.output
+
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert 'Show this message and exit.' in help_result.output
 
 
 
