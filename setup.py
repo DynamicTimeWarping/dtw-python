ee4af9a780f53f188b46128100c163ee15c70d05
Toni <toni.giorgino@gmail.com>
2019-08-29 21:50:09 +0200

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://DynamicTimeWarping.github.io',
-    version='0.3.4',
+    version='0.3.5',
     zip_safe=False,
 )
 
