2d83a7d65e6501c55d112105dd705313a4e0848f
Toni <toni.giorgino@gmail.com>
2019-08-30 09:35:29 +0200

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     cmdclass={'build_ext': Cython.Build.build_ext},
     test_suite='tests',
     url='https://DynamicTimeWarping.github.io',
-    version='0.3.6',
+    version='0.3.7',
     zip_safe=False,
 )
