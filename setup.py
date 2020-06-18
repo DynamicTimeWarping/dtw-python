72dc84263fd5b62f01628cbcf2d179c1bbd42bf9
Toni <toni.giorgino@gmail.com>
2020-06-18 21:18:38 +0200

    Bump version: 1.1.2 → 1.1.3

--- setup.py
+++ setup.py
@@ -68,6 +68,6 @@ setup(
     ext_modules=ext,
     # cmdclass={'build_ext': Cython.Build.build_ext},
     url='https://DynamicTimeWarping.github.io',
-    version='1.1.2',
+    version='1.1.3',
     zip_safe=False,
 )
