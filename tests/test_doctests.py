eb9c7537f0b63a440d086892a814e1bd05fc036c
Toni <toni.giorgino@gmail.com>
2020-06-18 21:06:21 +0200

    ci

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -7,19 +7,17 @@ import os.path
 import dtw
 
 
+def run_suite():
+    md = dtw.__path__[0]
+    fl = glob.glob(os.path.join(md, "*.py"))
+    suite = doctest.DocFileSuite(*fl, module_relative=False)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
 class TestDoctests(unittest.TestCase):
     def test_doctests(self):
-        unittest.TextTestRunner(verbosity=2).run(suite()) 
-        
+        run_suite()
 
-
-def suite():
-    md = dtw.__path__[0]
-    fl=glob.glob(os.path.join(md,"*.py"))
-    suite=doctest.DocFileSuite(*fl,module_relative=False)
-    return suite
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(suite()) 
-
-
+    run_suite()
