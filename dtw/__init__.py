1df443582c9f5da7a22922d769c5cfcf7b2170dc
Toni <toni.giorgino@gmail.com>
2022-01-14 18:18:19 +0100

    Bump version: 1.1.11 → 1.1.12

--- dtw/__init__.py
+++ dtw/__init__.py
@@ -9,7 +9,7 @@ main entry point.
 
 __author__ = """Toni Giorgino"""
 __email__ = 'toni.giorgino@gmail.com'
-__version__ = '1.1.11'
+__version__ = '1.1.12'
 
 # There are no comments in this package because it mirrors closely the R sources.
 
