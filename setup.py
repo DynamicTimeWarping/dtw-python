da28df77669130d93229dee7e05724d28c18f79c
Toni <toni.giorgino@gmail.com>
2022-07-27 15:18:20 +0200

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     ext_modules=cythonize([Extension('dtw._dtw_utils',
                  sources=['dtw/_dtw_utils.pyx', 'dtw/dtw_core.c'])]),
     url='https://DynamicTimeWarping.github.io',
-    version='1.2.1',
+    version='1.2.2',
     zip_safe=False,
 )
