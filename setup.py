9d013f79f1f8d828d7f6bb5ef7bdeb526bf2307c
Toni <toni.giorgino@gmail.com>
2024-05-27 11:48:56 +0200

    indent

--- setup.py
+++ setup.py
@@ -11,14 +11,15 @@ import numpy
 
 setup(
     include_package_data=True,
-    name='dtw-python',
+    name="dtw-python",
     #    packages=find_packages(include=['dtw']),
-    packages=['dtw'],
+    packages=["dtw"],
     include_dirs=numpy.get_include(),
-    ext_modules=cythonize([Extension('dtw._dtw_utils',
-                	             sources=['dtw/_dtw_utils.pyx', 'dtw/dtw_core.c'])],
-                	             force=True),
-    url='https://DynamicTimeWarping.github.io',
-    version='1.5.0',
+    ext_modules=cythonize(
+        [Extension("dtw._dtw_utils", sources=["dtw/_dtw_utils.pyx", "dtw/dtw_core.c"])],
+        force=True,
+    ),
+    url="https://DynamicTimeWarping.github.io",
+    version="1.5.0",
     zip_safe=False,
 )
